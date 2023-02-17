32e9767ccafd5579299a5efd01c8621759a3f960
Andrey Negrash <andi.ne2002@gmail.com>
2023-02-17 15:41:46 +0300

    Working lab

---
+++ request_tree.c
@@ -0,0 +1,5 @@
+//
+// Created by admin on 17.02.2023.
+//
+
+#include "request_tree.h"
---
+++ request_tree.h
@@ -0,0 +1,8 @@
+//
+// Created by admin on 17.02.2023.
+//
+
+#ifndef LLP2_REQUEST_TREE_H
+#define LLP2_REQUEST_TREE_H
+
+#endif //LLP2_REQUEST_TREE_H
